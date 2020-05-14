cc08e6d2db1a21687775a3c1d2c323244ea88917
Ivan Bolorino <ivan.bolorino@gmail.com>
2020-05-14 13:32:31 -0300

    Init

---
+++ quiz/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Question(models.Model):
+    question = models.CharField("Question", max_length=400)
+
+    def __str__(self):
+        return self.question
+
+
+class Answer(models.Model):
+    answer = models.CharField("Answer", max_length=255)
+    question = models.ForeignKey("Question", name="Question", on_delete=models.CASCADE, related_name="answers")
+    correct = models.BooleanField("Correct Answer")
+
+    def __str__(self):
+        return str(self.question.pk) + self.answer
+    
