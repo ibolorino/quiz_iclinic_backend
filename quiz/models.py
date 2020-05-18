a2c77c1dbf6313c5784366d73c03ddd26fa76db7
Ivan Bolorino <ivan.bolorino@gmail.com>
2020-05-18 14:45:19 -0300

    Creates functions to calculate accuracy

--- quiz/models.py
+++ quiz/models.py
@@ -5,13 +5,38 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question
+    
+    def has_correct_answer(self, pk):
+        return len(Answer.objects.filter(question__id=pk, correct=True)) > 0
+
+    def correct_answer(self, pk):
+        return Answer.objects.filter(question__id=pk, correct=True)
 
 
 class Answer(models.Model):
     answer = models.CharField("Answer", max_length=255)
-    question = models.ForeignKey("Question", name="Question", on_delete=models.CASCADE, related_name="answers")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers")
     correct = models.BooleanField("Correct Answer")
 
     def __str__(self):
         return self.answer
-    
+
+    def save(self, *args, **kwargs):
+        if self.correct == True:
+            if Question().has_correct_answer(self.question.pk):
+                return "Question already has a correct answer."
+        super(Answer, self).save(*args, **kwargs)
+
+    def get(self, pk):
+        try:
+            return Answer.objects.get(pk=pk)
+        except:
+            return "Answer not found."
+
+
+class QuizUser(models.Model):
+    name = models.CharField("Name", max_length=50)
+    has_answered = models.BooleanField("Has answered", default=False)
+
+    def __str__(self):
+        return self.name
---
+++ quiz/utils.py
@@ -0,0 +1,15 @@
+from .models import *
+
+def its_correct_answer(answer_id, question_id):
+    answer = Answer().get(answer_id)
+    if answer.correct == True:
+        if answer.question.id == question_id:
+            return True
+    return False
+
+def count_correct_answers(quiz):
+    correct_answers = 0
+    for question in quiz['quiz']:
+        if its_correct_answer(question['answer'], question['question']):
+            correct_answers += 1
+    return correct_answers
